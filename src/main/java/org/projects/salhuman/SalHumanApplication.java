d3b717f88b2af4fc932364035882617d5019586b
anass <chaffaaanass@gmail.com>
2024-04-08 00:15:50 +0000

    Entities Commit

---
+++ src/main/java/org/projects/salhuman/SalHumanApplication.java
@@ -0,0 +1,13 @@
+package org.projects.salhuman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalHumanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalHumanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/projects/salhuman/SalHumanApplicationTests.java
@@ -0,0 +1,13 @@
+package org.projects.salhuman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalHumanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
